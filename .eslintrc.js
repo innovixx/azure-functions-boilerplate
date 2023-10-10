b4a11423517eaf174884d90d2648e8fefbc4957c
Kalon Robson <kalon.robson@innovixx.co.uk>
2023-10-10 13:07:31 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    '@innovixx/eslint-config/configs/base',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+    'no-underscore-dangle': 'off',
+  },
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
+};
