74d8b37df228ede7bc88c501499c1dfb2f781c49
Kalon Robson <kalon.robson@innovixx.co.uk>
2024-07-04 11:15:56 +0100

    chore: update eslint-config to version 1.3.3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,13 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  extends: [
+    '@innovixx/eslint-config/configs/base',
+    '@innovixx/eslint-config/configs/typescript',
+  ],
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  extends: [
-    '@innovixx/eslint-config/configs/base',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.ts'],
-      },
-    },
-  },
   rules: {
-    'import/prefer-default-export': 'off',
-    'no-underscore-dangle': 'off',
+    'import/no-unresolved': 'off',
   },
-  overrides: [
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off',
-      },
-    },
-  ],
 };
